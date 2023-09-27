6e1b4ebeb77a51223397da53ab2f7e891c1c3015
AutumnCombs <combsau@mail.uc.edu>
2023-09-26 21:43:59 -0400

    Task 1 Done

--- src/SalesTax.java
+++ src/SalesTax.java
@@ -1,11 +1,17 @@
+import java.util.Arrays;
+
 public class SalesTax {
     public static void main(String[] args) {
 
-        double itemPrice = 5.00;
-        double salesTax = 0;
+        final double SALES_TAX_RATE = .05;
+        double itemPrice = 0;
+        double salesTaxAmt = 0;
 
-        salesTax = itemPrice * .05;
+        System.out.println("50.25");
+        itemPrice = 50.25;
 
-        System.out.println("The sales tax is " + itemPrice * salesTax);
+        salesTaxAmt = itemPrice * SALES_TAX_RATE;
+
+        System.out.println("The sales tax for the item price of $ " + itemPrice + " is $ " + salesTaxAmt + ".");
     }
 }
