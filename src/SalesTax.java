5433234b812437312851851a9c4f0855266c6a5f
AutumnCombs <combsau@mail.uc.edu>
2023-09-26 20:26:54 -0400

    Task 1 Done

---
+++ src/SalesTax.java
@@ -0,0 +1,11 @@
+public class SalesTax {
+    public static void main(String[] args) {
+
+        double itemPrice = 5.00;
+        double salesTax = 0;
+
+        salesTax = itemPrice * .05;
+
+        System.out.println("The sales tax is " + itemPrice * salesTax);
+    }
+}
